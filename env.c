4aec250b893808834aa4960c7ddea1ef9f7de9dc
khaoulaHamdouch <khaoulahamdouch01@gmail.com>
2023-05-24 17:08:43 -0700

    fix

---
+++ env.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _getenv - gets an env_var
+ * @name: name of the env_var
+ * Return: pointer to the env_var
+ */
+
+char *_getenv(char *name)
+{
+	char **env = NULL, *var = NULL, *value = NULL;
+	int i = 0, j = 0, len = 0;
+
+	env = environ;
+	while (env[i] != NULL)
+	{
+		var = env[i];
+		len = _strlen(name);
+		for (j = 0; j < len; j++)
+		{
+			if (name[j] != var[j])
+				break;
+		}
+		if (j == len && var[j] == '=')
+		{
+			value = var + len + 1;
+			break;
+		}
+		i++;
+	}
+	return (value);
+}
